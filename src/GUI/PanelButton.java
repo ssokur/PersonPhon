dbd3809e558b8638c015076b3ac0324bb724dc8a
ssokur <s.sokur@mail.ru>
2014-12-04 17:54:47 +0200

    Hibernate

--- src/GUI/PanelButton.java
+++ src/GUI/PanelButton.java
@@ -12,18 +12,16 @@ public class PanelButton extends JPanel
         JButton read    = new JButton("Read");
         JButton edit    = new JButton("Edit");
         JButton delete  = new JButton("Delete");
-        JButton lang    = new JButton("Lang");
 
         add(create);
         add(read);
         add(edit);
         add(delete);
-        add(lang);
 
         create. addActionListener(cmd.evCreate);
         read.   addActionListener(cmd.evRead);
         edit.   addActionListener(cmd.evUpdate);
         delete. addActionListener(cmd.evDelete);
-        lang.   addActionListener(cmd.exLang);
+
     }
 }
